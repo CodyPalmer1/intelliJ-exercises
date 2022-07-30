ef6920e18074526a737ea2cc9dda0bee24c6860d
Cody Palmer <codyryanpalmer@gmail.com>
2022-07-30 16:24:43 -0400

    added to july29th excercise

--- july29PracticeExercises/july29js/july29th.js
+++ july29PracticeExercises/july29js/july29th.js
@@ -1,9 +1,21 @@
 alert("Good afternoon.");
+<h1>ALL ABOUT YOU</h1> // Title of page.
+
+alert("Greetings " + userName + "!"); // Greet User.
+var userName = "";
+while (userName === ""){
+    userName = prompt("Hi, what is your name?");
+}
 
 var howAreYou = prompt("How are you doing today?");
 
-if(howAreYou.toLowerCase() == "good" || "great"){
+if (howAreYou.toLowerCase() == "good" || "great") {
     alert("Good! I am glad that you are doing well. Lets get started."); //Should run this line first
-} else (howAreYou !== "good" || "great");{
+} else (howAreYou !== "good" || "great");
+{
     alert("Okay, lets get started then."); //Should run this line second
 } // Need to figure out why code is running both lines instead of either or.
+
+<h2>Please answer the following questions.</h2>
+
+
