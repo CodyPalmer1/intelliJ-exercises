fac030463a5601cf8e5d63243a134cf9ce908d8f
Cody Palmer <codyryanpalmer@gmail.com>
2022-08-15 21:10:19 -0500

    updates to practice exercise

--- PracticeExercises/js/practiceExercise.js
+++ PracticeExercises/js/practiceExercise.js
@@ -24,27 +24,27 @@ if (howAreYou === "good") {
     alert("Okay then. Lets get started.");
 }
 
-var happinessScale = prompt("How happy are you today? Please enter a number from the list: " +
-    "1 - Great, 2 - Good, 3 - Okay, 4 - Horrible, 5 - Terrible.");
-switch (happinessScale){
-    case "1":
-        alert("That is awesome. I hope it continues!");
-        break;
-    case "2":
-        alert("That is good, hunt the good stuff!");
-        break;
-    case "3":
-        alert("It could always be worse, but I hope it gets better.");
-        break;
-    case "4":
-        alert("I am so sorry to hear that. Hopefully it gets better, and tomorrow is a great one.");
-        break;
-    case "5":
-        alert("At least days come to an end and two never repeat. I hope that tomorrow is nothing but great for you!");
-        break;
-    default:
-        alert("Invalid choice.");
-}
+// var happinessScale = prompt("How happy are you today? Please enter a number from the list: " +
+//     "1 - Great, 2 - Good, 3 - Okay, 4 - Horrible, 5 - Terrible.");
+// switch (happinessScale){
+//     case "1":
+//         alert("That is awesome. I hope it continues!");
+//         break;
+//     case "2":
+//         alert("That is good, hunt the good stuff!");
+//         break;
+//     case "3":
+//         alert("It could always be worse, but I hope it gets better.");
+//         break;
+//     case "4":
+//         alert("I am so sorry to hear that. Hopefully it gets better, and tomorrow is a great one.");
+//         break;
+//     case "5":
+//         alert("At least days come to an end and two never repeat. I hope that tomorrow is nothing but great for you!");
+//         break;
+//     default:
+//         alert("Invalid choice.");
+//}
 
 for(i = 0; i < 10; i++){
     var test = confirm("Will you hit 'Okay' 10 times?")
@@ -58,13 +58,13 @@ for(i = 0; i < 10; i++){
     }
 }
 
-var successLevels = ['Succeeding', 'Contentment', 'Complacency', 'Regression'];
-alert('There are many ways to rate your success level.');
-alert('A few may be: ');
-successLevels.forEach(function(successLevel) {
-    alert(successLevel);
-});
-alert (`Our goal is to try to help everyone get to ${successLevels[1]} or ${successLevels[2]}.`);
+// var successLevels = ['Succeeding', 'Contentment', 'Complacency', 'Regression'];
+// alert('There are many ways to rate your success level.');
+// alert('A few may be: ');
+// successLevels.forEach(function(successLevel) {
+//     alert(successLevel);
+// });
+// alert (`Our goal is to try to help everyone get to ${successLevels[1]} or ${successLevels[2]}.`);
 
 var you = {
     weight: 72,
