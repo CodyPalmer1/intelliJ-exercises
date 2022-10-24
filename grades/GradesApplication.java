65b49d505c49a562736f3812e3eee6824c4948fc
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-23 20:27:33 -0500

    Practicing Hashmaps and ArrayList

--- grades/GradesApplication.java
+++ grades/GradesApplication.java
@@ -1,6 +1,7 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -30,5 +31,50 @@ public class GradesApplication {
         students.put("MrCharles", charles);
         students.put("LameMartin", martin);
 
+        System.out.println("Welcome!");
+        System.out.println();
+        System.out.println("Here are the GitHub usernames of our students:");
+        System.out.println();
+        System.out.println("|CodyPalmer1|  |KingJames|  |MrCharles|  |LameMartin|");
+
+        boolean userContinue = false;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What student would you like to see more information on?");
+            String student = scanner.nextLine();
+
+            if (student.equalsIgnoreCase("CodyPalmer1")) {
+                System.out.printf("Name: %s%n", cody.getName());
+                System.out.printf("Github username: CodyPalmer1%n");
+                System.out.printf("Current average: %.2f%n", cody.getGradeAverage());
+                System.out.println();
+            } else if (student.equalsIgnoreCase("KingJames")) {
+                System.out.printf("Name: %s%n", james.getName());
+                System.out.printf("Github username: KingJames%n");
+                System.out.printf("Current average: %.2f%n", james.getGradeAverage());
+                System.out.println();
+            } else if (student.equalsIgnoreCase("MrCharles")) {
+                System.out.printf("NAME: %s%n", charles.getName());
+                System.out.printf("Github username: MrCharles%n");
+                System.out.printf("Current average: %.2f%n", charles.getGradeAverage());
+                System.out.println();
+            } else if (student.equalsIgnoreCase("LameMartin")) {
+                System.out.printf("Name: %s%n", martin.getName());
+                System.out.printf("Github username: LameMartin%n");
+                System.out.printf("Current average: %.2f%n", martin.getGradeAverage());
+                System.out.println();
+            } else if (!student.equalsIgnoreCase("CodyPalmer1") || !student.equalsIgnoreCase("KingJames") || !student.equalsIgnoreCase("MrCharles") || !student.equalsIgnoreCase("LameMartin")) {
+                System.out.printf("Sorry, no student found with the GitHub username of \"%s\"%n", student);
+            }
+
+            System.out.println("Would you like to see another student?");
+            String anotherStudent = scanner.nextLine();
+            if (anotherStudent.equalsIgnoreCase("y") || anotherStudent.equalsIgnoreCase("yes")) {
+                userContinue = true;
+            } else if (anotherStudent.equalsIgnoreCase("n") || anotherStudent.equalsIgnoreCase("no")) {
+                userContinue = false;
+                System.out.println("Goodbye, and have a wonderful day!");
+            }
+        } while (userContinue);
     }
 }
